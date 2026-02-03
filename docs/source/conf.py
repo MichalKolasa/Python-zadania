401f9d4e673c2ff2956bff02dd05109789e00c6a
michalkolasa <michalkolasa90@gmail.com>
2026-02-03 19:45:19 +0100

    Add Sphinx documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,7 @@ language = 'pl'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 autodoc_member_order = 'bysource'
