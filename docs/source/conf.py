9d00344eb194bb9996218e12abaecc6fa672f85c
michalkolasa <michalkolasa90@gmail.com>
2026-02-03 19:36:37 +0100

    Add Sphinx documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,7 +8,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath('../../Projekt_koncowy/'))
 
 project = 'Alien Invasion'
 copyright = '2026, Michał Kolasa'
